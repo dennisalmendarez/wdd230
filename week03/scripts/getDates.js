7d3606b62f491c48542ae885ea2a9cb1f5dc5a7b
dennisalmendarez <dennisyassid2212@hotmail.com>
2024-05-09 23:47:39 -0400

    week03

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var currentYear = new Date().getFullYear();
+    var content = 	"\u00A9" + currentYear + " Dennis Almendarez" + " Florida"
+    document.getElementById("year").textContent = content;
+
+    var lastModified = new Date(document.lastModified);
+    var formattedDate = lastModified.toLocaleDateString("en-US", {year: 'numeric', month: 'long', day: 'numeric', hour: "2-digit", minute: "2-digit", second: "numeric"});
+    document.getElementById("lastmodified").textContent = "Last Modified: " + formattedDate;
+});
